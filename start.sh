8231d9c4e0536d12a407e360803127f3ee9716f8
Mehmet Celik <mehmet@celik.be>
2019-09-20 15:25:59 +0200

    service files

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+PATH=$PATH:/home/sfxuser01/opt/node/current/bin:/home/sfxuser01/opt/yarn/current/bin
+#sfxResolve
+/home/sfxuser01/.config/yarn/global/node_modules/sfx-resolve/server.js
