437703f1e361ca33fc5a1a72c30dcfff33ba9d12
Mehmet Celik <mehmet@celik.be>
2020-03-23 15:31:28 +0100

    housekeeping

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+if [ -z "$1" ];then
+  OFFSET=0
+else
+  OFFSET=$1
+fi
+
+let "SPORT = 3000 + $OFFSET"
 PATH=$PATH:/home/sfxuser01/opt/node/current/bin:/home/sfxuser01/opt/yarn/current/bin
-#sfxResolve
-/home/sfxuser01/.config/yarn/global/node_modules/sfx-resolve/server.js
+export PORT=$SPORT && /home/sfxuser01/.config/yarn/global/node_modules/sfx-resolve/server.js
